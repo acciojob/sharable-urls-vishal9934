893a6902d19a3c9aa270b720c0941a5db3601688
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-02-16 15:07:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-// your code here
-var form = document.querySelector("form");
-var h3 = document.querySelector("h3");
-var nameELement = document.querySelector("#name");
-var yearElement = document.querySelector("#year");
-var btn = document.querySelector("#button");
-
-function handleSubmit(event) {
-  event.preventDefault();
-  var h3Value = "https://localhost:8080/";
-  var name = nameELement.value;
-  var year = yearElement.value;
-
-  if (name && year) {
-    h3Value += "?name=" + name + "&year=" + year;
-  } else if (name && !year) {
-    h3Value += "?name=" + name;
-  } else if (!name && year) {
-    h3Value += "?year=" + year;
-  }
-  h3.textContent = h3Value;
+function buttonClick() {
+	let name=document.getElementById("name").value
+    let year=document.getElementById("year").value
+	let url=document.getElementById("url")
+	
+	if (name!="" && year!="") {
+		url.innerText="https://localhost:8080/?name="+`${name}&year=${year}`
+	}
+    else if(name!=""){
+		url.innerText="https://localhost:8080/?name="+`${name}`
+	}
+    else if(year!=""){
+		url.innerText="https://localhost:8080/?year="+`${year}`
+	}
 }
-
-form.addEventListener("submit", handleSubmit);
